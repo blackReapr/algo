c3392a32979791031415b9e68cee66d3bf944714
magab0nd <mahammadea@code.edu.az>
2024-02-13 14:29:24 +0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿#region Task 1
+
+int num = 12;
+
+if  (num % 2 == 0)
+{
+    Console.WriteLine("cutdur");
+} else
+{
+    Console.WriteLine("tekdir");
+}
+#endregion
+
+#region Task 2
+int a = 13;
+if ( a % 21 == 0 )
+{
+    Console.WriteLine("bolunur");
+}else
+{
+    Console.WriteLine("bolunmur");
+}
+#endregion
+
+#region Task 3
+int n = 12;
+int m = 26;
+
+
+if ( n % 2 == 0 && m % 2 == 0)
+{
+    Console.WriteLine(n+m);
+}
+else
+{
+    Console.WriteLine("n ve ya m cut deyildir");
+}
+
+#endregion
+
+#region Task 4
+int temp = 17;
+if ( temp < 15)
+{
+    Console.WriteLine("Hava soyuqdur");
+} else
+{
+    Console.WriteLine("Hava istidir");
+}
+#endregion
